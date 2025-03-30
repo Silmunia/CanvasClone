71de00e71c31504cd9c0d2ab3a71a19e37321c63
Pedro Henrique Costa <62479229+Silmunia@users.noreply.github.com>
2025-03-30 16:08:35 -0300

    Refactors the canvas into a separate view

---
+++ CanvasClone/Views/CanvasView.swift
@@ -0,0 +1,40 @@
+//
+//  CanvasView.swift
+//  CanvasClone
+//
+//  Created by Pedro Henrique on 30/03/25.
+//
+
+import SwiftUI
+
+struct CanvasView: View {
+    
+    private let lineCount = max(3, Int(AppConstants.canvasWidth / AppConstants.canvasLineSpacing))
+    
+    var body: some View {
+        ScrollView([.horizontal, .vertical], showsIndicators: true) {
+            ZStack {
+                Color(.gray)
+                    .opacity(0.2)
+                    .frame(width: AppConstants.canvasWidth, height: AppConstants.canvasHeight)
+                
+                ForEach(0..<lineCount, id: \.self) { line in
+                    let xPosition = CGFloat(line) * (AppConstants.canvasWidth / CGFloat(lineCount))
+                    Rectangle()
+                        .fill(Color.black)
+                        .frame(width: 1, height: AppConstants.canvasHeight)
+                        .position(x: xPosition, y: 0.5 * AppConstants.canvasHeight)
+                }
+            }
+        }
+        .frame(width: AppConstants.canvasFrameWidth, height: AppConstants.canvasFrameHeight)
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: 10)
+        .padding()
+    }
+}
+
+#Preview {
+    CanvasView()
+}
