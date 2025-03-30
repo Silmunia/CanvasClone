170422ab62e614bdb7efa554d0c5d11b67d8aaac
Pedro Henrique Costa <62479229+Silmunia@users.noreply.github.com>
2025-03-30 15:29:11 -0300

    Initial Commit

---
+++ CanvasClone/CanvasCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasCloneApp.swift
+//  CanvasClone
+//
+//  Created by Pedro Henrique on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CanvasCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CanvasCloneTests/CanvasCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasCloneTests.swift
+//  CanvasCloneTests
+//
+//  Created by Pedro Henrique on 30/03/25.
+//
+
+import Testing
+@testable import CanvasClone
+
+struct CanvasCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
