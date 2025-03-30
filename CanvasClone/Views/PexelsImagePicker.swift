f3933e52b6de5cad9f0d7ffecaf9fc9da4bf8d38
Pedro Henrique Costa <62479229+Silmunia@users.noreply.github.com>
2025-03-30 16:33:30 -0300

    Adds handling URL error in the API request

--- CanvasClone/Views/PexelsImagePicker.swift
+++ CanvasClone/Views/PexelsImagePicker.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct PexelsImagePicker: View {
+    
+    @State private var fetchErrorMessage: String?
+    
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let message = fetchErrorMessage {
+                    Text(message).foregroundColor(.gray)
+                }
             }
             .navigationTitle("Pick an Image")
+            .task {
+                await fetchPexelsImages()
+            }
+        }
+    }
+    
+    private func fetchPexelsImages() async {
+        let apiKey = ""
+        let urlString = ""
+        
+        guard let url = URL(string: urlString) else {
+            fetchErrorMessage = "Invalid URL request"
+            return
         }
     }
 }
