80ec63544aaf3182960947650cb418204eb0720d
Pedro Henrique Costa <62479229+Silmunia@users.noreply.github.com>
2025-03-30 20:33:00 -0300

    Adds entity that stores information about selected images to display

---
+++ CanvasClone/Model/CanvasImage.swift
@@ -0,0 +1,14 @@
+//
+//  CanvasImage.swift
+//  CanvasClone
+//
+//  Created by Pedro Henrique on 30/03/25.
+//
+
+import SwiftUI
+
+struct CanvasImage: Identifiable {
+    let id: UUID
+    var uiImage: UIImage
+    var position: CGPoint
+}
