9a57dae589948230980e62f9e5579461701463bf
Pedro Henrique Costa <62479229+Silmunia@users.noreply.github.com>
2025-03-30 20:47:22 -0300

    Adds entity that will display each selected image

---
+++ CanvasClone/Views/DraggableImage.swift
@@ -0,0 +1,16 @@
+//
+//  DraggableImage.swift
+//  CanvasClone
+//
+//  Created by Pedro Henrique on 30/03/25.
+//
+
+import SwiftUI
+
+struct DraggableImage: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
