1c7e3ef26645a9d28f062bec4472fb4c0774d35c
Pedro Henrique Costa <62479229+Silmunia@users.noreply.github.com>
2025-03-30 15:47:34 -0300

    Adds constant values for canvas size and frame

---
+++ CanvasClone/AppConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstants.swift
+//  CanvasClone
+//
+//  Created by Pedro Henrique on 30/03/25.
+//
+
+import SwiftUI
+
+struct AppConstants {
+    static let canvasWidth: CGFloat = 800
+    static let canvasHeight: CGFloat = 400
+    
+    static let canvasFrameWidth: CGFloat = 400
+    static let canvasFrameHeight: CGFloat = 400
+}
