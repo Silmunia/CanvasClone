743de140fa2e181d21de898fb4f2871c6d1f899a
Pedro Henrique Costa <62479229+Silmunia@users.noreply.github.com>
2025-03-30 23:07:55 -0300

    Refactors image sizing logic to consider image scale

--- CanvasClone/ViewModels/CanvasImagesViewModel.swift
+++ CanvasClone/ViewModels/CanvasImagesViewModel.swift
@@ -31,45 +31,45 @@ class CanvasImagesViewModel: ObservableObject {
         canvasImages.append(newImage)
     }
     
-    func updateImagePosition(imageID: UUID, newPosition: CGPoint) {
+    func updateImagePosition(imageID: UUID, newPosition: CGPoint, imageScale: CGFloat) {
         if let index = canvasImages.firstIndex(where: { $0.id == imageID }) {
             var updatedImage = canvasImages[index]
             
-            updatedImage.position = applyBoundsToPosition(position: newPosition)
+            updatedImage.position = applyBoundsToPosition(position: newPosition, imageScale: imageScale)
             
             canvasImages[index] = updatedImage
         }
     }
     
-    private func applyBoundsToPosition(position: CGPoint) -> CGPoint {
-        let imageBorders = calculateImageBorders(imagePosition: position)
+    private func applyBoundsToPosition(position: CGPoint, imageScale: CGFloat) -> CGPoint {
+        let imageBorders = calculateImageBorders(imagePosition: position, imageScale: imageScale)
         var newX: CGFloat = position.x
         var newY: CGFloat = position.y
         
         if (imageBorders.left < 0 || imageBorders.right < 0) {
-            newX = 0.5 * AppConstants.basicImageSize
+            newX = 0.5 * AppConstants.basicImageSize * imageScale
         }
         
         if (imageBorders.left > AppConstants.canvasWidth || imageBorders.right > AppConstants.canvasWidth) {
-            newX = AppConstants.canvasWidth - (0.5 * AppConstants.basicImageSize)
+            newX = AppConstants.canvasWidth - (0.5 * AppConstants.basicImageSize * imageScale)
         }
         
         if (imageBorders.top < 0 || imageBorders.bottom < 0) {
-            newY = 0.5 * AppConstants.basicImageSize
+            newY = 0.5 * AppConstants.basicImageSize * imageScale
         }
         
         if (imageBorders.top > AppConstants.canvasHeight || imageBorders.bottom > AppConstants.canvasHeight) {
-            newY = AppConstants.canvasHeight - (0.5 * AppConstants.basicImageSize)
+            newY = AppConstants.canvasHeight - (0.5 * AppConstants.basicImageSize * imageScale)
         }
         
         return CGPoint(x: newX, y: newY)
     }
     
-    func updateSnapping(imageID: UUID, currentPosition: CGPoint) -> (horizontal: CGFloat?, vertical: CGFloat?) {
+    func updateSnapping(imageID: UUID, currentPosition: CGPoint, imageScale: CGFloat) -> (horizontal: CGFloat?, vertical: CGFloat?) {
         
         var newSnappingLines: (horizontal: CGFloat?, vertical: CGFloat?) = (nil, nil)
         
-        let imageBorders = calculateImageBorders(imagePosition: currentPosition)
+        let imageBorders = calculateImageBorders(imagePosition: currentPosition, imageScale: imageScale)
         
         if imageBorders.left < snapThreshold {
             newSnappingLines.vertical = 0
@@ -89,16 +89,16 @@ class CanvasImagesViewModel: ObservableObject {
         }
         
         for other in canvasImages where other.id != imageID {
-            let otherBorders = calculateImageBorders(imagePosition: other.position)
+            let otherBorders = calculateImageBorders(imagePosition: other.position, imageScale: imageScale)
             
-            if abs(currentPosition.y - other.position.y) < (0.5 * AppConstants.basicImageSize) {
+            if abs(currentPosition.y - other.position.y) < (0.5 * AppConstants.basicImageSize * imageScale) {
                 if abs(imageBorders.left - otherBorders.right) < snapThreshold {
                     newSnappingLines.vertical = otherBorders.right
                 } else if abs(imageBorders.right - otherBorders.left) < snapThreshold {
                     newSnappingLines.vertical = otherBorders.left
                 }
             }
-            if abs(currentPosition.x - other.position.x) < (0.5 * AppConstants.basicImageSize) {
+            if abs(currentPosition.x - other.position.x) < (0.5 * AppConstants.basicImageSize * imageScale) {
                 if abs(imageBorders.top - otherBorders.bottom) < snapThreshold {
                     newSnappingLines.horizontal = otherBorders.bottom
                 } else if abs(imageBorders.bottom - otherBorders.top) < snapThreshold {
@@ -110,11 +110,11 @@ class CanvasImagesViewModel: ObservableObject {
         return newSnappingLines
     }
     
-    private func calculateImageBorders(imagePosition: CGPoint) -> (top: CGFloat, right: CGFloat, bottom: CGFloat, left: CGFloat) {
-        let imageLeft = imagePosition.x - (0.5 * AppConstants.basicImageSize)
-        let imageRight = imagePosition.x + (0.5 * AppConstants.basicImageSize)
-        let imageTop = imagePosition.y - (0.5 * AppConstants.basicImageSize)
-        let imageBottom = imagePosition.y + (0.5 * AppConstants.basicImageSize)
+    private func calculateImageBorders(imagePosition: CGPoint, imageScale: CGFloat) -> (top: CGFloat, right: CGFloat, bottom: CGFloat, left: CGFloat) {
+        let imageLeft = imagePosition.x - (0.5 * AppConstants.basicImageSize * imageScale)
+        let imageRight = imagePosition.x + (0.5 * AppConstants.basicImageSize * imageScale)
+        let imageTop = imagePosition.y - (0.5 * AppConstants.basicImageSize * imageScale)
+        let imageBottom = imagePosition.y + (0.5 * AppConstants.basicImageSize * imageScale)
         
         return (top: imageTop, right: imageRight, bottom: imageBottom, left: imageLeft)
     }
@@ -124,10 +124,8 @@ class CanvasImagesViewModel: ObservableObject {
         && abs(imagePosition.y - AppConstants.canvasHeight * 0.5) < snapThreshold
     }
     
-    func applySnapping(imageID: UUID, snappingTarget: (horizontal: CGFloat?, vertical: CGFloat?), currentPosition: CGPoint) {
-        
-        let imageBorders = calculateImageBorders(imagePosition: currentPosition)
-        
+    func applySnapping(imageID: UUID, snappingTarget: (horizontal: CGFloat?, vertical: CGFloat?), currentPosition: CGPoint, imageScale: CGFloat) {
+                
         guard let index = canvasImages.firstIndex(where: { $0.id == imageID }) else {
             return
         }
@@ -135,40 +133,40 @@ class CanvasImagesViewModel: ObservableObject {
         var updatedImage = canvasImages[index]
         
         if let snapX = snappingTarget.vertical {
-            if isImageSnappingVerticallyToCanvas(imagePosition: currentPosition) {
+            if isImageSnappingVerticallyToCanvas(imagePosition: currentPosition, imageScale: imageScale) {
                 updatedImage.position.x = snapX
             } else if currentPosition.x >= snapX {
-                updatedImage.position.x = snapX + (0.5 * AppConstants.basicImageSize)
+                updatedImage.position.x = snapX + (0.5 * AppConstants.basicImageSize * imageScale)
             } else {
-                updatedImage.position.x = snapX - (0.5 * AppConstants.basicImageSize)
+                updatedImage.position.x = snapX - (0.5 * AppConstants.basicImageSize * imageScale)
             }
         }
         
         if let snapY = snappingTarget.horizontal {
-            if isImageSnappingHorizontallyToCanvas(imagePosition: currentPosition) {
+            if isImageSnappingHorizontallyToCanvas(imagePosition: currentPosition, imageScale: imageScale) {
                 updatedImage.position.y = snapY
             } else if currentPosition.y >= snapY {
-                updatedImage.position.y = snapY + (0.5 * AppConstants.basicImageSize)
+                updatedImage.position.y = snapY + (0.5 * AppConstants.basicImageSize * imageScale)
             } else {
-                updatedImage.position.y = snapY - (0.5 * AppConstants.basicImageSize)
+                updatedImage.position.y = snapY - (0.5 * AppConstants.basicImageSize * imageScale)
             }
         }
         
         canvasImages[index] = updatedImage
     }
     
-    private func isImageSnappingVerticallyToCanvas(imagePosition: CGPoint) -> Bool {
+    private func isImageSnappingVerticallyToCanvas(imagePosition: CGPoint, imageScale: CGFloat) -> Bool {
         
-        let imageBorders = calculateImageBorders(imagePosition: imagePosition)
+        let imageBorders = calculateImageBorders(imagePosition: imagePosition, imageScale: imageScale)
         
         return imageBorders.left < snapThreshold
             || abs(imageBorders.right - AppConstants.canvasWidth) < snapThreshold
         || isNearCanvasCenter(imagePosition: imagePosition)
     }
     
-    private func isImageSnappingHorizontallyToCanvas(imagePosition: CGPoint) -> Bool {
+    private func isImageSnappingHorizontallyToCanvas(imagePosition: CGPoint, imageScale: CGFloat) -> Bool {
         
-        let imageBorders = calculateImageBorders(imagePosition: imagePosition)
+        let imageBorders = calculateImageBorders(imagePosition: imagePosition, imageScale: imageScale)
         
         return imageBorders.top < snapThreshold
             || abs(imageBorders.bottom - AppConstants.canvasHeight) < snapThreshold
